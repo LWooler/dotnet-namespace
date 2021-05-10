67398340c816ca6464cc5b9d8611dc5bbc49f30a
LWooler <lwooler@att.net>
2021-05-10 13:44:09 -0500

    added FluentAssertions library

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using FluentAssertions;
+
+namespace dotnet_namespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myExample = new exampleClass();
+            myExample.additionMethod();
+            Console.WriteLine("Hello FluentAssertions!");
+        }
+        public class exampleClass {
+        public void additionMethod() {
+            var first = 3;
+            var second = 7;
+            var result = first + second;
+
+            result.Should().Be(10);
+        }
+    }
+
+
+}
+}
